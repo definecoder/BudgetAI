0e770e22e0c6385df3320e6c0931812ec98be72a
shawon-majid <shawonmajid@gmail.com>
2024-05-28 00:06:16 +0600

    user model updated

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -3,16 +3,20 @@ import mongoose from "mongoose";
 // write user schema here
 export const userSchema = new mongoose.Schema({
   name: String,
-  email: String,
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email is required"],
+  },
   password: String,
   avatar: String,
   dailyGoal: Number,
   monthlyGoal: Number,
-  yearlyGoal: Number,  
+  yearlyGoal: Number,
   createdAt: {
     type: Date,
     default: Date.now,
-  },  
+  },
 });
 
 export default mongoose.model("User", userSchema);
