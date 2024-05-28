699b155924e85339d3c6750b0cca7c4083067787
shawon-majid <shawonmajid@gmail.com>
2024-05-28 23:47:47 +0600

    login signup api connected

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -6,52 +6,68 @@ import { Textarea } from "@/components/ui/textarea"
 import { DataTableDemo } from "./myTable";
 import {PaperPlaneIcon} from '@radix-ui/react-icons'
 import { cookies} from 'next/headers';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 export type User = {
     name: string;
     email: string;
+    dailyGoal?: number,
+    monthlyGoal?: number,
+    yearlyGoal?: number,
 }
 
-export default function Page() {
-    // before loading this page I need to check the token.
-    // const token = localStorage.getItem('token');
-
-    
-
+export default function Page() {    
+    const [user, setUser] = useState<User | null>(null);
     useEffect(()=>{
         const token = localStorage.getItem('token');
-    
-        console.log(token);
-
+        async function fetchUser(){
+            try{
+                const response = await axios.get('http://localhost:3000/users/loggedin', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                console.log(response.data);
+                setUser(response.data);
+            }
+            catch(e){
+                alert(JSON.stringify(e));
+            }            
+        }
+        if(token){
+            fetchUser();
+        }
+        else{
+            alert('You are not logged in');
+        }
     }, [])
 
-
-    const currentUser = 'kuddus';
-    return (
+    
+    
+    return !user ? (<> <div>Youre not logged in</div> </>) : (
         <main>
             {/* main wrapper */}
             <div className="flex  h-[80vh] justify-around">
                 {/* left wrapper */}
                 <div className=" flex flex-col justify-center h-full w-[40vw]">
                     {/* upper */}
-                    <div className=" flex flex-col border-2">
-                        <h1 className="self-start mb-20">Have a good day, <b className="text-secondary">{currentUser}!</b></h1>
+                    <div className=" flex flex-col">
+                        <h1 className="self-start mb-20">Have a good day, <b className="text-secondary">{user && user['name']}!</b></h1>
                         <Label className="text-primary mb-5 text-3xl">Your Expense</Label>
                         <Textarea className="bg-transparent border-secondary" placeholder="What did you spend money on today?" />
                         <Button className=" bg-transparent mt-5 border-secondary border-2 w-[30%] max-w-[100px] self-end"> <PaperPlaneIcon />  </Button>
                     </div>
 
                     {/* lower */}
-                    <div className="border-2">
+                    {/* <div className="border-2">
                         Analytics
-                    </div>
+                    </div> */}
                 </div>
                 {/* right wrapper */}
                 <div className="hidden md:block">
                     <DataTableDemo />
-                
                 </div>
             </div>
         </main>
--- client/app/login/loginform.tsx
+++ client/app/login/loginform.tsx
@@ -11,46 +11,71 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import axios from "axios"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 export function LoginForm() {
+
+  const router = useRouter();
 
   const [login, setLogin] = useState({  
     email: '',
     password: ''
   });
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setLogin({ ...login, [id]: value });
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const response = await axios.post('http://localhost:3000/auth/login', login)
+    if(response.statusText === 'OK'){
+      localStorage.setItem('token', response.data.token);
+      router.push("/dashboard");
+    }
+  }
+
 
   return (
-    <Card className="w-full max-w-sm bg-transparent border-secondary">
-      <CardHeader>
-        <CardTitle className="text-2xl text-primary">Login</CardTitle>
-        <CardDescription className=" text-white">
-          Enter your email below to login to your account.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid gap-2 ">
-          <Label className="text-primary" htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-        </div>
-        <div className="grid gap-2 ">
-          <Label className="text-primary" htmlFor="password">Password</Label>
-          <Input id="password" type="password" required />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full text-white bg-transparent border-2 border-secondary">Sign in</Button>
+    <form onSubmit={handleSubmit}>
+      <Card className="w-full max-w-sm bg-transparent border-secondary">
         
-      </CardFooter>
-      <CardFooter>
-        <Button className="w-full text-white bg-transparent border-2 border-secondary">Login with Google</Button>
-      </CardFooter>
-      <CardFooter className=" text-white">
-        <p>Don&apos;t Have an Account? (</p>  <Link className="text-primary underline " href={'/signup'}> Create One!</Link>)
-      </CardFooter>
+        <CardHeader>
+          <CardTitle className="text-2xl text-primary">Login</CardTitle>
+          <CardDescription className=" text-white">
+            Enter your email below to login to your account.
+          </CardDescription>
+        </CardHeader>
 
-    </Card>
+        
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2 ">
+            <Label className="text-primary" htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com" onChange={handleChange} required />
+          </div>
+          <div className="grid gap-2 ">
+            <Label className="text-primary" htmlFor="password">Password</Label>
+            <Input id="password" type="password" onChange={handleChange} required />
+          </div>
+        </CardContent>
+        
+        <CardFooter>
+          <Button className="w-full text-white bg-transparent border-2 border-secondary" type="submit">Sign in</Button>
+          
+        </CardFooter>
+        <CardFooter>
+          <Button className="w-full text-white bg-transparent border-2 border-secondary">Login with Google</Button>
+        </CardFooter>
+        <CardFooter className=" text-white">
+          <p>Don&apos;t Have an Account? (</p>  <Link className="text-primary underline " href={'/signup'}> Create One!</Link>)
+        </CardFooter>
+        
+
+      </Card>
+    </form>
   )
 }
